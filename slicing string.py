da9e6ef332fa141754df7b6a1c772153d8f676dc
mithun96-art <157538757+mithun96-art@users.noreply.github.com>
2024-01-23 17:01:56 -0500

    Add files via upload

---
+++ slicing string.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 09:47:10 2023
+
+@author: mithun sai
+"""
+# this coding created by mithun sai 
+# this code about is Slicing string
+#SLICING
+b = "Hello,World!"
+print(b[2:5])
+b = "Hello,World!"
+print(b[5])
+#Slice From the Start
+b = "Hello,World!"
+print(b[:5])
+b = "Hello,World!"
+print(b[5:])
+#Slice To the End
+b = "Hello, World!"
+print(b[2:])
+b = "Hello,World!"
+print(b[:2])
+b = "Hello,World!"
+print(b[2])
+b = "Hello, World!"
+#Negative Indexing
+print(b[-5:-2])
+b = "Hello, World!"
+print(b[-5:+2])
+b = "Hello, World!"
+print(b[5:2])
+
